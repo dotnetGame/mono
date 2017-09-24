d9351ac89eea0bdd59e4738b3a24557865fc66e4
姚旭 <lia77593@163.com>
2017-09-24 13:50:48 +0800

    丢失VS2010相关兼容代码，保证VS2017编译通过，去除部分警告

--- mono/utils/mono-compiler.h
+++ mono/utils/mono-compiler.h
@@ -133,12 +133,12 @@
 
 /* Deal with Microsoft C compiler differences */
 #ifdef _MSC_VER
-
+#include <math.h>
 #include <float.h>
-#define isnan(x)	_isnan(x)
+
+#if _MSC_VER <= 1600
 #define trunc(x)	(((x) < 0) ? ceil((x)) : floor((x)))
-#define isinf(x)	(_isnan(x) ? 0 : (_fpclass(x) == _FPCLASS_NINF) ? -1 : (_fpclass(x) == _FPCLASS_PINF) ? 1 : 0)
-#define isnormal(x)	_finite(x)
+#endif
 
 #define popen		_popen
 #define pclose		_pclose
